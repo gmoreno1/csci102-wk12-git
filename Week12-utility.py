210817adcd4be22fab96bbc15aebeb1ea1c8da90
unknown <Gabriella Moreno>
2019-11-21 22:59:38 -0700

    added UpdateString function

--- Week12-utility.py
+++ Week12-utility.py
@@ -10,4 +10,17 @@ def LoadFile(file):
     contents = f.readlines()
     print('OUTPUT', contents)
 
+# this code has bugs
+
+def UpdateString(string_1, a, num):
+    s_1_list = []
+    
+    for i in len(string_1):
+        s_1_list.append(string_1[i])
+    
+
+    s_1_list[num] = a
+    final_string = s_1_list.join()
+    print('OUTPUT', final_string)
+
 
